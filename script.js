686699641ea3010cb1f1682b6e0a3aff9b7f5c79
Manish9699 <manishmahadadalkar18@gmail.com>
2023-07-21 16:35:31 +0530

    Game logic

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 "use strict";
 
-document.querySelector(".check").addEventListener("click");
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number entered! ";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too high!";
+    score--;
+    document.querySelector(".score").textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too low!";
+    score--;
+    document.querySelector(".score").textContent = score;
+  }
+});
